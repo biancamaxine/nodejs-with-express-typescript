7477c4e75ddc34fdec81ef6ce5bf60a37dd1c837
biancamaxine <bianca.maxine.dev@gmail.com>
2023-04-25 05:30:34 -0400

    feature: ADD Routes IN routes/index.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App is running at port ${PORT}`);
